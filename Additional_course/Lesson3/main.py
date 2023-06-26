39ab0084e393800004bdb6e1e37ac180e590d1f0
Ivan Tanajlov <wasserleu@gmail.com>
2023-06-27 02:09:54 +0300

    First task in HW4 and simple game in Additional_course

--- Additional_course/Lesson3/main.py
+++ Additional_course/Lesson3/main.py
@@ -2,7 +2,42 @@ import os
 from random import randint
 clear = lambda: os.system('cls')
 clear()
+# 60
+# print('Think of the number.')
+# input()
+# user_number = None
+# min_num_value, max_num_value = 1, 100
+# while True: 
+#   num = randint(min_num_value, max_num_value + 1)
+#   print(num)
+#   user_answer = input()
+#   if user_answer == 'yes':
+#     user_number = num
+#     print('Victory!', user_number)
+#     break
+#   elif user_answer == '>':
+#     min_num_value = num + 1
+#   else:
+#     max_num_value = num
+computer_num = randint(1, 101)
+difficulty = int(input('Choose the difficult(number of try): '))
+print(computer_num)
+my_number = int(input('Number: '))
+i = 1
+while my_number != computer_num:
+    if my_number > computer_num:
+        my_number = int(input('You need a smaller number: '))
+    else:
+        my_number = int(input('You need a bigger number: '))
+    i += 1
+    if i == difficulty:
+         print('You lose big boy!!!')
+         break
+else:
+        print('You win!')
+     
 
+'''
 lst1 = [i for i in range(7, 1, -1)]
 print(lst1)
 dictin1 = set(lst1)
@@ -11,3 +46,4 @@ print(dictin1)
 lst2 = [(x, y, z) for x in range(1, 5) for y in range(5, 18) for z in range(2,5)]
 
 print(lst2, len(lst2))
+'''
---
+++ HW/HW4/main.py
@@ -0,0 +1,98 @@
+''' Задача 22: Даны два неупорядоченных набора целых чисел (может быть, с повторениями). 
+Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
+Пользователь вводит 2 числа. n — кол-во элементов первого множества. 
+m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств. '''
+
+import os
+from random import randint
+clear = lambda: os.system('cls')
+clear()
+
+# Решение через множества:
+'''size1 = int(input('Enter the size of the first set: '))
+size2 = int(input('Enter the size of the second set: '))
+
+print('Enter the values for first set: ')
+set1 = {int(input()) for i in range(size1)}
+print('Enter the values for second set: ')
+set2 = {int(input()) for i in range(size2)}
+
+print('First set: ', *set1) 
+print('Second set: ', *set2)
+set_result = set1 & set2
+# Как я понял, если нет отрицательного элемента во множестве, 
+# то оно автоматом сортируется от меньшего к большему. Если есть, то сортируется сначало 
+# положительные элементы, а потом идут тоже отсортированные отрицательные элементы. Но лучше всё равно 
+# использовать sorted(), т. к. иногда почему-то он не сортирутеся как надо.
+set_result = sorted(set_result)
+print(*set_result)'''
+
+
+# Решение через списки:
+size1 = int(input('Enter the size of the first set: '))
+size2 = int(input('Enter the size of the second set: '))
+list1 = []
+list2 = []
+print('Enter the values for first list:')
+i = 0
+while i < size1:
+  while True:
+    element = input()
+    if element[0] == '-':
+       element = element[1:]
+    if element.isdigit():
+       element = int(element)
+       break
+    else:
+       print('You enter incorrect number, try again', end=': ')
+  if list1.__contains__(element):
+      size1 -= 1
+      continue
+  else:
+      list1.append(element)
+  i += 1
+
+print('Enter the values for second list:')
+i = 0
+while i < size2:
+  while True:
+    element = input()
+    if element[0] == '-':
+       element = element[1:]
+    if element.isdigit():
+       element = int(element)
+       break
+    else:
+       print('You enter incorrect number, try again', end=': ')
+  if list2.__contains__(element):
+      size2 -= 1
+      continue
+  else:
+      list2.append(element)
+  i += 1
+
+print(f'First sequence: {list1}') 
+print(f'Second sequence: {list2}')
+
+result_list = []
+if len(list1) < len(list2):
+   for el in list1:
+      if el in list2:
+         result_list.append(el)
+         i = len(result_list) - 1
+         while i != 0:
+            if result_list[i - 1] > result_list[i]:
+               result_list[i - 1], result_list[i] = result_list[i], result_list[i - 1]
+            i -= 1
+else:
+   for el in list2:
+      if el in list1:
+         result_list.append(el)
+         i = len(result_list) - 1
+         while i != 0:
+            if result_list[i - 1] > result_list[i]:
+               result_list[i - 1], result_list[i] = result_list[i], result_list[i - 1]
+            i -= 1
+
+print('Result sequence', end=': ')
+print(*result_list, sep=', ')
