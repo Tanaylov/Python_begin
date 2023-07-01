991ef3383f637552033e1166d20e097ae21bab21
Ivan Tanajlov <wasserleu@gmail.com>
2023-07-01 15:14:21 +0300

    Seminar 5 and lection - Recursiy

---
+++ Lection/Lection_3_function/main.py
@@ -0,0 +1,67 @@
+import os
+from random import randint
+clear = lambda: os.system('cls')
+clear()
+# import modul1
+# from modul1 import max1
+# from modul1 import * # Import all functions
+import modul1 as m1 # программно переименовали файл для удобства
+print(m1.max1(3, 11))
+# print(modul1.max1(5, 7))
+
+# Функция суммы от одного до N:
+number1 = 9
+def sumFrom1ToNumber(number, word = 'Hello'): 
+# Можно по умолчанию передать переменной какое-то значение и при вызове можно это значение изменить
+    print(word)
+    result_sum = 0
+    for i in range(1, number + 1):
+        result_sum += i
+        # print(result_sum)
+    return result_sum
+
+res = sumFrom1ToNumber(number1, 'Result: ')
+print('tot')
+print(res)
+
+# В функцию можно передать неограниченное количество аргумернтов с иомощью '*':
+def string(*args):
+    result = ''
+    for i in args:
+        result += str(i)
+    return result
+
+print(string(1, 5, 3))
+        
+# Рекурсия:
+def fibonachi(num):
+    if num in [1, 2]:
+        return 1
+    else:
+        return fibonachi(num - 1) + fibonachi(num - 2)
+
+print(fibonachi(9))
+
+print('--------------------------')
+
+a = c = 3
+b = 3
+
+def aToExtentOfb (a, b):
+    if b == 0:
+        print(a)
+        return a
+    else:
+        print(a, b)
+        return a*a + aToExtentOfb(a, b - 1)
+
+aToExtentOfb(a, b)
+
+# def sumAB (num1, num2):
+#     if num2 == 0:
+#         print(num1)
+#         return num1
+#     else:
+#         return sumAB(num1 = num1 + 1, num2 = num2 - 1)
+    
+# sumAB(a, b)
